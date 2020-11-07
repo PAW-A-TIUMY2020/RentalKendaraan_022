43cb261add84c34c554365b1578c7273fa9d2e2f
ReAlpha39 <yunusw718@gmail.com>
2020-11-07 13:15:30 +0800

    add form field validator

--- RentalKendaraan_022/Models/Customer.cs
+++ RentalKendaraan_022/Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace RentalKendaraan_022.Models
 {
@@ -9,15 +10,25 @@ namespace RentalKendaraan_022.Models
         {
             Peminjaman = new HashSet<Peminjaman>();
         }
-
+    
         public int IdCustomer { get; set; }
+        [Required(ErrorMessage = "Nama customer tidak boleh kosong")]
         public string NamaCustomer { get; set; }
+        [Required(ErrorMessage = "NIK customer tidak boleh kosong")]
+        [RegularExpression("^[0-9]*$", ErrorMessage = "Hanya boleh diisi dengan angka")]
         public string Nik { get; set; }
+        [Required(ErrorMessage = "Alamat tidak boleh kosong")]
         public string Alamat { get; set; }
+        [Required(ErrorMessage = "No HP tidak boleh kosong")]
+        [MinLength(10, ErrorMessage = "No HP minimal 10 digit")]
+        [MaxLength(13, ErrorMessage = "No HP maksimal 13 digit")]
         public string NoHp { get; set; }
+        [Required(ErrorMessage = "Gender tidak boleh kosong")]
         public int? IdGender { get; set; }
 
         public Gender IdGenderNavigation { get; set; }
         public ICollection<Peminjaman> Peminjaman { get; set; }
+
+        
     }
 }
