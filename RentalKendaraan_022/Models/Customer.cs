5eeb7a7ec2074a0df371ff8d97e44ff09c3d50e7
ReAlpha39 <yunusw718@gmail.com>
2020-11-07 13:36:12 +0800

    Add another validator to all form field

--- RentalKendaraan_022/Models/Customer.cs
+++ RentalKendaraan_022/Models/Customer.cs
@@ -20,6 +20,7 @@ namespace RentalKendaraan_022.Models
         [Required(ErrorMessage = "Alamat tidak boleh kosong")]
         public string Alamat { get; set; }
         [Required(ErrorMessage = "No HP tidak boleh kosong")]
+        [RegularExpression("^[0-9]*$", ErrorMessage = "Hanya boleh diisi dengan angka")]
         [MinLength(10, ErrorMessage = "No HP minimal 10 digit")]
         [MaxLength(13, ErrorMessage = "No HP maksimal 13 digit")]
         public string NoHp { get; set; }
--- RentalKendaraan_022/Models/KondisiKendaraan.cs
+++ RentalKendaraan_022/Models/KondisiKendaraan.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace RentalKendaraan_022.Models
 {
@@ -11,6 +12,7 @@ namespace RentalKendaraan_022.Models
         }
 
         public int IdKondisi { get; set; }
+        [Required(ErrorMessage = "Kondisi tidak boleh kosong")]
         public string NamaKondisi { get; set; }
 
         public ICollection<Pengembalian> Pengembalian { get; set; }
