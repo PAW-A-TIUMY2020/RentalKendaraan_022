093a6faed458ab493c2d8d7cbae903648ce51f51
ReAlpha39 <yunusw718@gmail.com>
2020-11-28 12:48:15 +0800

    add upload file feature

--- RentalKendaraan_022/Startup.cs
+++ RentalKendaraan_022/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
@@ -10,6 +11,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 
 namespace RentalKendaraan_022
 {
@@ -35,6 +37,10 @@ namespace RentalKendaraan_022
             services.AddDbContext<Models.RentalKendaraanContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DevConnection")));
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddSingleton<IFileProvider>(
+              new PhysicalFileProvider(
+                  Path.Combine(Directory.GetCurrentDirectory(), "wwwroot")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
